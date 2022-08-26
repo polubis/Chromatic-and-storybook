1aa01ffdf517a8e1f4402f7edbd6244baf7b816a
Adrian Polubinski <adrian.polubinski@billennium.com>
2022-08-26 11:26:38 +0200

    Add semantic improvements

--- src/ui/input-field/InputField.stories.tsx
+++ src/ui/input-field/InputField.stories.tsx
@@ -6,11 +6,7 @@ export default {
   title: "InputField",
 } as Meta;
 
-// InputFieldProps interface tells for storybook which properties will be added to addons - also types of props are inferred.
-// They will be merged later with properties defined in .args property.
-// It's not smart enough to determine properties from extended interfaces.
 const Template: Story<InputFieldProps> = (args) => {
-  // Here you can add additional logic if needed.
   return (
     <div style={{ padding: "16px", background: "#101b48" }}>
       <InputField {...args} />
@@ -23,6 +19,5 @@ Playground.args = {};
 
 export const WithPlaceholder = Template.bind({});
 WithPlaceholder.args = {
-  // Adding this prop tells for storybook - "add this property with the value to addons".
   placeholder: "Type something...",
 };
